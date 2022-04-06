5a51af2bf9c195d6b9c4d72d8df21845731746b1
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-04-06 03:55:25 -0300

    Restart Game and Win Implemented

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,21 @@
+let clickCount = 0;
+let cardQuantity = 14;
+let won = false;
+
+function restartGame() {
+  if (prompt("Deseja reiniciar o jogo?") === "sim") {
+    document.querySelectorAll(".found").forEach((element) => {
+      element.classList.remove("found");
+      clickCount = 0;
+      won = false;
+    });
+  }
+}
+
+function getFoundCount() {
+  return document.querySelectorAll(".found").length;
+}
+
 function getFlipped() {
   return document.querySelector(".flip");
 }
@@ -26,21 +44,36 @@ function flipBack(card1, card2) {
 }
 
 function selectCard(element) {
-  const flipped = getFlipped();
-  element.classList.add("flip");
-  if (flipped && flipped !== element) {
-    const elementImage = getBackImage(element);
-    const flippedImage = getBackImage(flipped);
-    if (elementImage === flippedImage) {
-      pairFound(element, flipped);
-    } else {
-      pairNotFound(element, flipped);
-      window.setTimeout(flipBack, 1000, element, flipped);
+  // Evita bug no clickCount caso clique muito rápido, também evita de selecionar uma carta que está virando
+  if (!element.classList.contains("found") && !element.classList.contains("temp")) {
+    const flipped = getFlipped();
+    element.classList.add("flip");
+
+    if (flipped !== element) {
+      if (flipped) {
+        const elementImage = getBackImage(element);
+        const flippedImage = getBackImage(flipped);
+        if (elementImage === flippedImage) {
+          pairFound(element, flipped);
+        } else {
+          pairNotFound(element, flipped);
+          window.setTimeout(flipBack, 1000, element, flipped);
+        }
+      }
+
+      if (!won) {
+        clickCount++;
+        if (getFoundCount() === cardQuantity) {
+          won = true;
+          window.setTimeout(alert, 500, `Você ganhou em ${clickCount} rodadas`);
+          window.setTimeout(restartGame, 500);
+        }
+      }
     }
   }
 }
 
-function validadeQuantity(quantity) {
+function validateQuantity(quantity) {
   if (!quantity) {
     return false;
   }
