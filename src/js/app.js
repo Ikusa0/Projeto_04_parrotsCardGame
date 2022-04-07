6a62b3408f5e52538bf7d4661cac4a8be709a8d6
ayrtonporto <22572595+Ikusa0@users.noreply.github.com>
2022-04-06 23:09:04 -0300

    Card Distribution Implemented

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-// --------------------------- STOPWATCH --------------------------
+// =========================== STOPWATCH ==========================
 const HTML_SECONDS = document.querySelector(".seconds");
 const HTML_MILLISECONDS = document.querySelector(".milliseconds");
 let seconds = 0;
@@ -28,32 +28,19 @@ function resetTimer() {
   stopTimer();
   seconds = 0;
   milliseconds = 0;
-  HTML_SECONDS.innerHTML = `${seconds}`.padStart(2, "0");
-  HTML_MILLISECONDS.innerHTML = `${milliseconds}`.padStart(2, "0");
+  HTML_SECONDS.innerHTML = "00";
+  HTML_MILLISECONDS.innerHTML = "00";
 }
-// ----------------------------------------------------------------
+// ================================================================
 
-// ---------------------- VARIÁVEIS GLOBAIS -----------------------
+// ====================== VARIÁVEIS GLOBAIS =======================
 let clickCount = 0;
-let cardQuantity = 14;
-let won = false;
-// ----------------------------------------------------------------
+let cardQuantity;
+// ================================================================
 
-function restartGame() {
-  if (prompt("Deseja reiniciar o jogo?") === "sim") {
-    document.querySelectorAll(".found").forEach((element) => {
-      element.classList.remove("found");
-      clickCount = 0;
-      won = false;
-    });
-
-    resetTimer();
-    startTimer();
-  }
-}
-
-function getFoundCount() {
-  return document.querySelectorAll(".found").length;
+// ======================== AUX FUNCTIONS =========================
+function winner() {
+  return document.querySelectorAll(".found").length === cardQuantity;
 }
 
 function getFlipped() {
@@ -83,6 +70,41 @@ function flipBack(card1, card2) {
   card2.classList.remove("temp");
 }
 
+function resetCardBoard(cardBoard) {
+  cardBoard.innerHTML = "";
+}
+
+function insertCard(cardBoard, src) {
+  cardBoard.innerHTML += `\
+  <li onclick="selectCard(this)" class="card">
+    <div class="front-face face">
+      <img src="./src/img/front.png" alt="" />
+    </div>
+    <div class="back-face face">
+       <img src="${src}" alt="" />
+    </div>
+  </li>`;
+}
+
+function comparator() {
+  return Math.random() - 0.5;
+}
+
+function validateQuantity(quantity) {
+  if (!quantity) {
+    return false;
+  }
+  if (quantity % 2 === 1) {
+    return false;
+  }
+  if (quantity < 4 || quantity > 14) {
+    return false;
+  }
+  return true;
+}
+// ================================================================
+
+// ====================== ON-CLICK FUNCTIONS ======================
 function selectCard(element) {
   // Evita bug no clickCount caso clique muito rápido, também evita de selecionar uma carta que está virando
   if (!element.classList.contains("found") && !element.classList.contains("temp")) {
@@ -101,39 +123,64 @@ function selectCard(element) {
         }
       }
 
-      if (!won) {
-        clickCount++;
-        if (getFoundCount() === cardQuantity) {
-          stopTimer();
-          won = true;
-          window.setTimeout(
-            alert,
-            500,
-            `Você ganhou em ***${clickCount}*** rodadas!\nTempo gasto: ***${seconds}s e ${milliseconds}ms***`
-          );
-          window.setTimeout(restartGame, 500);
-        }
+      clickCount++;
+
+      if (winner()) {
+        stopTimer();
+        window.setTimeout(
+          alert,
+          500,
+          `Você ganhou em ***${clickCount}*** rodadas!\nTempo gasto: ***${seconds}s e ${milliseconds}ms***`
+        );
+        window.setTimeout(restartGame, 500);
       }
     }
   }
 }
+// ================================================================
 
-// function validateQuantity(quantity) {
-//   if (!quantity) {
-//     return false;
-//   }
-//   if (quantity % 2 === 1) {
-//     return false;
-//   }
-//   if (quantity < 4 || quantity > 14) {
-//     return false;
-//   }
-//   return true;
-// }
+// ===================== MAIN FUNCTIONALITIES =====================
+function restartGame() {
+  if (prompt("Deseja reiniciar o jogo? (sim / não)") === "sim") {
+    document.querySelectorAll(".found").forEach((element) => {
+      element.classList.remove("found");
+    });
+    clickCount = 0;
+    resetTimer();
+    window.setTimeout(startGame, 500);
+  }
+}
 
-// let cardQuantity;
-// do {
-//   cardQuantity = parseInt(prompt("Digite a quantidade de cartas (4 à 14):"));
-// } while (!validadeQuantity(cardQuantity));
+function randomizeCards(quantity) {
+  const cardBoard = document.querySelector("ul");
+  let images = [
+    "./src/img/bobrossparrot.gif",
+    "./src/img/explodyparrot.gif",
+    "./src/img/fiestaparrot.gif",
+    "./src/img/metalparrot.gif",
+    "./src/img/revertitparrot.gif",
+    "./src/img/tripletsparrot.gif",
+    "./src/img/unicornparrot.gif",
+  ]
+    .sort(comparator)
+    .slice(0, quantity / 2);
+  images = images.concat(images).sort(comparator);
 
-startTimer();
+  resetCardBoard(cardBoard);
+
+  for (let i = 0; i < quantity; i++) {
+    insertCard(cardBoard, images[i]);
+  }
+}
+
+function startGame() {
+  do {
+    cardQuantity = parseInt(prompt("Digite a quantidade de cartas (4 à 14):"));
+  } while (!validateQuantity(cardQuantity));
+
+  randomizeCards(cardQuantity);
+  startTimer();
+}
+// ================================================================
+
+startGame();
